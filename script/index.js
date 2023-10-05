8dbe9bee8dd38505d399631b72767dfe9f2be72c
RpMoreira98 <rodrigopontes126@gmail.com>
2023-10-05 16:04:02 -0300

    Mudança de código do JS

--- script/index.js
+++ script/index.js
@@ -12,15 +12,19 @@ form.addEventListener('submit', (e) => {
     const day = parseInt(dayInp, 10);
     const month = parseInt(monthIpn, 10) - 1;
     const year = parseInt(yearInp, 10);
-    let input = new Date(year, month, day)
-    let date = new Date();;
+    let input = new Date(year, month, day);
+    let date = new Date();
     const dayResult = parseInt((date - input) / (1000 * 60 * 60 * 24), 10);
-    const yearResult = parseInt(dayResult / 365)
+    const yearResult = parseInt(dayResult / 365);
     const monthResult = month <= date.getMonth() ? parseInt(date.getMonth() - month) : date.getMonth();
 
     if (input) {
         dayOut.innerHTML = dayResult;
         monthOut.innerHTML = monthResult;
         yearOut.innerHTML = yearResult;
+
+        document.getElementById('day').value = "null";
+        document.getElementById('month').value = "null";
+        document.getElementById('year').value = "null";
     } 
 })
